56d57aec5615e634146c39ca4b5b9dc67bd514a3
Arthur Schang <arthur.c.schang2.ctr@mail.mil>
2021-03-31 13:41:28 -0400

    Updates to documentation and testing for the required name field.

--- mapping_relay/test/test_pcl2.py
+++ mapping_relay/test/test_pcl2.py
@@ -2,16 +2,17 @@
 
 import rospy
 import random
+import sys
 
 from sensor_msgs import point_cloud2
 from sensor_msgs.msg import PointCloud2, PointField
 from std_msgs.msg import Header
 
 
-def test_cloud():
-	publisher = rospy.Publisher('slam/cloud', PointCloud2, queue_size=10)
-	rospy.init_node('test_pcl2')
-	rate = rospy.Rate(5)
+def test_cloud(topic):
+	publisher = rospy.Publisher(topic, PointCloud2, queue_size=10)
+	rospy.init_node('test_pcl2', anonymous=True)
+	rate = rospy.Rate(0.5)
 	while not rospy.is_shutdown():
 		header = Header()
 		header.stamp = rospy.Time.now()
@@ -24,7 +25,7 @@ def test_cloud():
 
 		points = []
 		for i in range(1000):
-			point = [random.random() * 10, random.random() * 10, random.random()*10]
+			point = [(random.random()-0.5) * 10, (random.random()-0.5) * 10, (random.random()-0.5) * 10]
 			points.append(point)
 
 		cloudmsg = point_cloud2.create_cloud(header, fields, points)
@@ -33,8 +34,11 @@ def test_cloud():
 		rate.sleep()
 
 if __name__ == '__main__':
-	try:
-		test_cloud()
-	except rospy.ROSInterruptException:
-		pass
+  topic = 'slam/cloud'
+  if len(sys.argv) > 1:
+    topic = sys.argv[1]
+  try:
+    test_cloud(topic)
+  except rospy.ROSInterruptException:
+    pass
 
